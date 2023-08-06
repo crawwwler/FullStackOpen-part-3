19c7409d71485dfde817a0952dc5c9ad18f80bba
crawwwler <shahinkopite@gmail.com>
2023-08-06 21:08:28 +0330

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://mypbapp.fly.dev/
---
+++ index.js
@@ -0,0 +1,131 @@
+const express = require('express') // IMPORT EXPRESS
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express() // ASSIGN AN EXPRESS APP TO THE APP VARIABLE
+
+
+app.use(express.json()) // WE GET JSON OF EXPRESS INTO THE USE
+app.use(cors())
+
+morgan.token('data-sent', (request) => {
+    return JSON.stringify(request.body)
+})
+
+app.use(morgan(':method :url :status :response-time ms - :data-sent'))
+
+
+
+// OUR DATA COLLECTION AT THE MOMENT
+let data = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+// FUNCTION TO GENERATE ID's
+
+const generateID = () => {
+    const min = 10
+    const max = 999
+    return Math.floor(Math.random() * (max - min + 1) + min )
+}
+
+
+//ROUTES
+
+app.get("/", (request, response) => {
+    response.send('<h1>Hello you!</h1>')
+})
+
+
+app.get("/api/info", (request, response) => {
+    const xhelper = `Phonebook has info for ${data.length} people`
+    const options = {
+        weekday: 'long',
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        timeZoneName: 'long'
+    }
+    const currentTime = new Date().toLocaleString('en-US', options)
+    //console.log(currentTime)
+    response.send(`<h4>${xhelper}</h4>
+                <br/><h4>${currentTime}</h4>`)
+})
+
+app.get("/api/persons", (request, response) => {
+    response.json(data)
+})
+
+
+app.get("/api/persons/:id", (request, response)=> {
+    //IMPORTANT => CONVERT STRING TO INTEGER WHILE WE RETRIEVE THE ID
+    const id = Number(request.params.id)
+    const pTor = data.find(person => person.id === id)
+    if (pTor){ // IF THERE WAS NO MATCHING ID , pTor WOULD BE NULL.
+        response.json(pTor)
+    }else {
+        response.status(404).end()
+    }
+})
+
+app.post('/api/persons', (request, response)=> {
+    const body = request.body
+    if(!body.name || !body.number){
+        return response.status(400).json({
+            error: "name or number is missing"
+        })
+    }
+
+    const dupName = data.find(person => person.name === body.name)
+    if(dupName){
+        return response.status(400).json({
+            error: " name must be unique"
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateID()
+    }
+
+    data = data.concat(person)
+    response.json(person)
+})
+
+
+app.delete('/api/persons/:id', (request, response)=>{
+    const id = Number(request.params.id)
+    data = data.filter(person => person.id !== id)
+
+    response.status(204).end()
+
+})
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+})
