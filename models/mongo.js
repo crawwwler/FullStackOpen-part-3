2f7f80413512214f1156a1b34bd448e77ad91c0d
crawwwler <shahinkopite@gmail.com>
2023-08-08 17:56:33 +0330

    getting real

--- models/mongo.js
+++ models/mongo.js
@@ -1,48 +1,30 @@
 const mongoose = require('mongoose')
 
-if(process.argv.length<3){
-    console.log('password missing')
-    process.exit(1)
-}
-
-const pass = process.argv[2]
-
-const url = `mongodb+srv://shahinkopite:${pass}@fsopen.cg2fblb.mongodb.net/phonebook?retryWrites=true&w=majority`
-
 mongoose.set('strictQuery', false)
-mongoose.connect(url, {
-    connectTimeoutMS: 30000
-})
+
+const url = process.env.MONGODB_URI
+
+console.log(`connecting to ${url}`)
+
+mongoose.connect(url)
+    .then(result=> {
+        console.log('connected to MongoDB')
+    })
+    .catch(error => {
+        console.log(`connection to MongoDB has stopped due to ${error.message}`)
+    })
 
 const entrySchema = new mongoose.Schema({
     name: String,
     number: String
 })
 
-const Entry = mongoose.model("Person", entrySchema)
-
-if(process.argv.length<5){
-    console.log("phonebook:")
-    Entry.find({}).then(persons=> {
-        persons.forEach(person=> {
-            console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close()
-        process.exit(1)
-    })
-    return
-}
-
-const person = new Entry({
-    name: process.argv[3],
-    number: process.argv[4]
+entrySchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
 })
 
-person.save().then(result=> {
-    console.log(`added ${person.name} number ${person.number} to phonebook`)
-    mongoose.connection.close()
-})
-
-
-
-
+module.exports = mongoose.model('Person', entrySchema)
